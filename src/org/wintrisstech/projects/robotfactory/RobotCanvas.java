4bebd32b479ca4a8ee78e88c215d9272a2aa9ad4
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-05-20 12:15:49 -0700

    Show many robots

--- src/org/wintrisstech/projects/robotfactory/RobotCanvas.java
+++ src/org/wintrisstech/projects/robotfactory/RobotCanvas.java
@@ -4,13 +4,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import javax.swing.JComponent;
 
+/**
+ * This class is a JComponent that draws a lot of Robots.
+ */
 class RobotCanvas extends JComponent {
-    private Robot r;
+
+    private static final int GRID_SIZE = 200;
+
+    private Robot[] r = new Robot[12];
 
     public RobotCanvas() {
-        r = new Robot();
+        for (int i = 0; i < r.length; i++) {
+            r[i] = new Robot();
+        }
     }
 
     @Override
@@ -20,10 +29,20 @@ class RobotCanvas extends JComponent {
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 800, 600);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, 800, 600);
 
-        r.draw((Graphics2D)g);
+        for (int row = 0; row < 3; row++) {
+            g2.setTransform(AffineTransform.getTranslateInstance(0*GRID_SIZE, row*GRID_SIZE));
+            r[0+row*4].draw(g2);
+            g2.setTransform(AffineTransform.getTranslateInstance(1*GRID_SIZE, row*GRID_SIZE));
+            r[1+row*4].draw(g2);
+            g2.setTransform(AffineTransform.getTranslateInstance(2*GRID_SIZE, row*GRID_SIZE));
+            r[2+row*4].draw(g2);
+            g2.setTransform(AffineTransform.getTranslateInstance(3*GRID_SIZE, row*GRID_SIZE));
+            r[3+row*4].draw(g2);
+        }
     }
 
 }
